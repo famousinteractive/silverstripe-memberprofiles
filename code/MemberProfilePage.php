3c873bcc8e74e63118e74ba5463adfe73bfa24fb
famousinteractive <famousinteractive@gmail.com>
2016-12-14 17:22:59 +0100

    Update MemberProfilePage.php
    Update data to send all the member data

--- code/MemberProfilePage.php
+++ code/MemberProfilePage.php
@@ -827,10 +827,7 @@ class MemberProfilePage_Controller extends Page_Controller {
                 $email->send();
             }
         } elseif($this->EmailType == 'ValidationUnder16') {
-            $data = new \stdClass();
-
-            $data->Email = $member->ParentEmail;
-
+            $data = $member;
             $data->Link  = Director::absoluteURL(Controller::join_links (
                 $this->Link('confirm'),
                 $member->ID,
